e7d9476641de22e984ef39916f580c7bd4893cb4
𝕄𝕣.𝔸𝕟𝕒𝕟𝕒𝕤 <118059207+Dolens-Mortem@users.noreply.github.com>
2026-01-27 17:30:39 +0300

    Update users.py

--- Parcer4Academy/users.py
+++ Parcer4Academy/users.py
@@ -8,12 +8,11 @@ user_account = {}
 async def user_init(user_id):
     if user_id not in user_account:
         download_dir = f"files/{user_id}"
-        file_path = os.path.join(download_dir, "excel-file")
         os.makedirs(download_dir, exist_ok=True)
         user_account[user_id] = {
             "language" : Language("ru"),
             "isEnter" : Defender(),
-            "excelParcer" : ExcelParcer(file_path),
+            "excelParcer" : ExcelParcer(download_dir),
             "isFileLoaded" : False
         }
     print(user_account)
@@ -27,6 +26,14 @@ def get_entering(user_id):
 def get_excel_parcer(user_id):
     return user_account[user_id]["excelParcer"]
 
+def get_file_loaded(user_id):
+    return user_account[user_id]["isFileLoaded"]
+
 def set_file_loaded(user_id, condition: bool):
     user_account[user_id]["isFileLoaded"] = condition
+
+def delete_excel_file(user_id):
+    download_dir = f"files/{user_id}"
+    os.remove(f"{download_dir}/excel-file")
+    user_account[user_id]["isFileLoaded"] = False
 
